9f83f20fc37a4331d83fa7c2c98fc7c15ce0c4c8
Aleksei Burmistrov <mr.ehbr@gmail.com>
2024-06-30 19:20:10 +0300

    wiop

--- craco.config.js
+++ craco.config.js
@@ -1,6 +1,4 @@
 const CopyPlugin = require("copy-webpack-plugin");
-const path = require("path");
-const fs = require("fs");
 
 module.exports = {
   webpack: {
@@ -10,32 +8,6 @@ module.exports = {
           // Use copy plugin to copy *.wasm to output folder.
           patterns: [{ from: "node_modules/onnxruntime-web/dist/*.wasm", to: "static/js/[name][ext]" }],
         }),
-        {
-          apply: (compiler) => {
-            compiler.hooks.beforeRun.tapAsync("ListModelsPlugin", (compilation, callback) => {
-              const modelDir = path.resolve(__dirname, "public/model");
-              const models = fs.readdirSync(modelDir).filter((file) => file.endsWith(".onnx"));
-
-              console.log("Models found:", models);
-              // Create a string representing the array of models in JavaScript format
-              const modelsArray = JSON.stringify(models);
-
-              // Path to the React component
-              const reactComponentPath = path.resolve(__dirname, "src/App.js");
-
-              // Read the React component
-              let componentCode = fs.readFileSync(reactComponentPath, "utf-8");
-
-              // Replace the hard-coded models array
-              componentCode = componentCode.replace(/modelFiles = \[.*\];/, `modelFiles = ${modelsArray};`);
-
-              // Write back the updated component code
-              fs.writeFileSync(reactComponentPath, componentCode, "utf-8");
-
-              callback();
-            });
-          },
-        },
       ],
     },
     configure: (config) => {
